b99b4ee6077e69afcc5d1274338e558d31bb3b99
cafetesting <cryptocafe852@gmail.com>
2025-12-06 21:41:38 +0800

    Add Vercel Speed Insights integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 import '../styles/globals.css'
 
@@ -14,7 +15,10 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				{children}
+				<SpeedInsights />
+			</body>
 		</html>
 	)
 }
